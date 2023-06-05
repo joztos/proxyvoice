32e668e65d2cbbea7862aae0614afe2f92eb7d53
Your Name <you@example.com>
2023-06-04 22:19:35 -0600

    naw

---
+++ api/detectIntent.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { SessionsClient } = require('dialogflow').v2beta1;
+
+const app = express();
+app.use(express.json());
+
+// Referenciando las variables de entorno
+const projectId = process.env.DIALOGFLOW_PROJECT_ID;
+const sessionId = process.env.DIALOGFLOW_SESSION_ID;
+const languageCode = process.env.DIALOGFLOW_LANGUAGE_CODE;
+
+const sessionClient = new SessionsClient();
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+app.post('/detectIntent', async (req, res) => {
+  const { queryInput } = req.body;
+
+  const request = {
+    session: sessionPath,
+    queryInput: queryInput,
+  };
+
+  try {
+    const responses = await sessionClient.detectIntent(request);
+    const result = responses[0].queryResult;
+    res.send(result);
+  } catch (err) {
+    console.error('Dialogflow request failed', err);
+    res.status(500).send(err);
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Bienvenido a mi servidor proxy para Dialogflow');
+});
+
+module.exports = app; // Exportar la aplicación Express
